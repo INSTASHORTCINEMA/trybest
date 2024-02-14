800e3e496cc5eed1a0e5fa2067375d67983a3ed3
INSTASHORTCINEMA <155711729+INSTASHORTCINEMA@users.noreply.github.com>
2024-02-14 19:23:56 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/instashortcinema/trybest.git /trybest
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /trybest
+fi
+cd /trybest
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
